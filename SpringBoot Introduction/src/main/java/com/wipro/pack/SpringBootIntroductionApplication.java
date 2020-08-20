64e3fe4a2a72cf3961102e7e91e799bf5a5ed13b
Sandhya8888 <36587087+Sandhya8888@users.noreply.github.com>
2020-08-20 18:00:06 +0530

    Add files via upload

---
+++ SpringBoot Introduction/src/main/java/com/wipro/pack/SpringBootIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntroductionApplication.class, args);
+	}
+
+}
